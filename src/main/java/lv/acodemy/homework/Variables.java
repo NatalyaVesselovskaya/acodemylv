9b20ffce01dc28bdbd673c67f8319c5ab036dc63
Natalya <vesselovskayan@gmail.com>
2022-11-08 18:38:53 +0200

    [1_homework]: Add homework

---
+++ src/main/java/lv/acodemy/homework/Variables.java
@@ -0,0 +1,347 @@
+package lv.acodemy.homework;
+
+public class Variables {
+    public static void main(String[] args) {
+
+        String task1 = "\nTask 1: Create 10 variables for each data type\n";
+        System.out.println(task1);
+
+        String byte1 = "Task 1.1. byte\n";
+        System.out.println(byte1);
+
+        byte price1 = 100;
+        System.out.println("Price=" + price1);
+
+        byte price2 = 67;
+        System.out.println(price2);
+
+        byte price3 = 33;
+        System.out.println(price3);
+
+        byte price4 = 88;
+        System.out.println(price4);
+
+        byte price5 = 122;
+        System.out.println(price5);
+
+        byte discount1 = -50;
+        System.out.println("Your discount is" + discount1 + "%");
+
+        byte discount2 = -30;
+        System.out.println("Your discount is" + discount2 + "%");
+
+        byte discount3 = -25;
+        System.out.println("Your discount is" + discount3 + "%");
+
+        byte discount4 = -15;
+        System.out.println("Your discount is" + discount4 + "%");
+
+        byte discount5 = -10;
+        System.out.println("Your discount is" + discount5 + "%");
+
+        String short1 = "\nTask 1.2. short\n";
+        System.out.println(short1);
+
+        short salary1 = 1000;
+        System.out.println("My first salary:" + salary1);
+
+        short salary2 = 2000;
+        System.out.println("My second salary:" + salary2);
+
+        short salary3 = 3500;
+        System.out.println(salary3);
+
+        short salary4 = 5700;
+        System.out.println(salary4);
+
+        short rent1 = 15000;
+        System.out.println("One bedroom apartment rent is " + rent1 + " euro per month");
+
+        short rent2 = 17100;
+        System.out.println(rent2);
+
+        short savings1 = 24750;
+        System.out.println(savings1);
+
+        short savings2 = 30280;
+        System.out.println(savings2);
+
+        short cardBalance1 = -29000;
+        System.out.println("Card balance:" + cardBalance1);
+
+        short cardBalance2 = -32768;
+        System.out.println("Card balance:" + cardBalance2);
+
+        String char1 = "\nTask 1.3. char\n";
+        System.out.println(char1);
+
+        char categoryA = 'A';
+        System.out.println("Product category - " + categoryA);
+
+        char categoryB = 'B';
+        System.out.println(categoryB);
+
+        char categoryC = 'C';
+        System.out.println(categoryC);
+
+        char seatD = 'D';
+        System.out.println("Plane seat " + seatD);
+
+        char seatE = 'E';
+        System.out.println("Theater seat " + seatE);
+
+        char seatF = 'F';
+        System.out.println(seatF);
+
+        char seatG = 'G';
+        System.out.println(seatG);
+
+        char myGrade1 = 'A';
+        System.out.println("Exam grade:" + myGrade1);
+
+        char myGrade2 = 'B';
+        System.out.println("Home task grade " + myGrade2 + "-");
+
+        char myGrade3 = 'C';
+        System.out.println("Test grade: " + myGrade3);
+
+        String int1 = "\nTask 1.4. int\n";
+        System.out.println(int1);
+
+        int populationRiga = 632614;
+        System.out.println("Population of Riga is " + populationRiga);
+
+        int populationStockholm = 975551;
+        System.out.println("Population of Stockholm is " + populationStockholm);
+
+        int populationRome = 2873000;
+        System.out.println("Population of Rome is " + populationRome + " in 2017");
+
+        int populationParis = 2188000;
+        System.out.println("Population of Paris is " + populationParis);
+
+        int populationLondon = 8797000;
+        System.out.println("Population of London is " + populationLondon + " in 2017");
+
+        int refugeesN = 15000;
+        System.out.println("Number of refugees exceeds " + refugeesN + " people");
+
+        int shares = 7743;
+        System.out.println("Share rate:" + shares);
+
+        int coronaCases = 637680480;
+        System.out.println("Coronavirus Cases:" + coronaCases);
+
+        int coronaDeaths = 6605415;
+        System.out.println("Coronavirus death rate:" + coronaDeaths);
+
+        int coronaRec = 617201317;
+        System.out.println("Recovery cases:" + coronaRec);
+
+        String float1 = "\nTask 1.5. float\n";
+        System.out.println(float1);
+
+        float temp1 = 36.6f;
+        System.out.println("Normal temperature:" + temp1 + "C");
+
+        float temp2 = 37.8f;
+        System.out.println("Fever:" + temp2 + "C");
+
+        float temp3 = 39.8f;
+        System.out.println("Extremely high temperature:" + temp3 + "C");
+
+        float tempAvrg = 10.5f;
+        System.out.println("Average temperature in autumn is +" + tempAvrg + "C");
+
+        float tempAvrg2 = 22.7f;
+        System.out.println("Average temperature in summer is +" + tempAvrg2 + "C");
+
+        float tempAvrg3 = 7.7f;
+        System.out.println("-" + tempAvrg3);
+
+        float testScoreMath = 97.4f;
+        System.out.println("Math test: " + testScoreMath + "%");
+
+        float testScoreEng = 64.5f;
+        System.out.println("English test: " + testScoreEng + "%");
+
+        float testScoreEcon = 75.9f;
+        System.out.println("Economics test: " + testScoreEcon + "%");
+
+        float testScoreHist = 88.3f;
+        System.out.println("History test: " + testScoreHist + "%");
+
+        String long1 = "\nTask 1.6. long\n";
+        System.out.println(long1);
+
+        long cellsNmbr = 30000000000L;
+        System.out.println("Number of cells in the human body is about " + cellsNmbr);
+
+        long distSun = 150000000L;
+        System.out.println("Earth's average distance to the Sun is " + distSun + " km");
+
+        long distUranus = 2600000000L;
+        System.out.println("Distance between the Earth and the Uranus is " + distUranus + " km");
+
+        long populationEarth = 7837000000L;
+        System.out.println("Population of the Earth is " + populationEarth + " people");
+
+        long shares1 = 4500000600L;
+        System.out.println(shares1);
+
+        long shares2 = 650000000L;
+        System.out.println(shares2);
+
+        long shares3 = 8000000010L;
+        System.out.println(shares3);
+
+        long shares4 = 20000000040L;
+        System.out.println(shares4);
+
+        long nerves = 7000000000L;
+        System.out.println("Number of nerves in the human body is " + nerves);
+
+        long seconds = 31536000;
+        System.out.println("One year equals " + seconds + " seconds");
+
+        String double1 = "\nTask 1.7. double\n";
+        System.out.println(double1);
+
+        double stockPrice1 = 23748.99;
+        System.out.println(stockPrice1);
+
+        double stockPrice2 = 45356.09;
+        System.out.println(stockPrice2);
+
+        double stockPrice3 = 65.0876;
+        System.out.println(stockPrice3);
+
+        double stockPrice4 = 1234.45509;
+        System.out.println(stockPrice4);
+
+        double stockPrice5 = 98.90034;
+        System.out.println(stockPrice5);
+
+        double share6 = 2065.98;
+        System.out.println(share6);
+
+        double share7 = 548932.0001;
+        System.out.println(share7);
+
+        double share8 = 23298.32;
+        System.out.println(share8);
+
+        double share9 = 4202.432;
+        System.out.println(share9);
+
+        double share10 = 987323.0029;
+        System.out.println(share10);
+
+        String boolean1 = "\nTask 1.8. boolean\n";
+        System.out.println(boolean1);
+
+        boolean isSunday = false;
+        System.out.println(isSunday);
+
+        boolean isFriday = true;
+        System.out.println(isFriday);
+
+        boolean isWinter = false;
+        System.out.println(isWinter);
+
+        boolean isSummer = false;
+        System.out.println(isSummer);
+
+        boolean isAutumn = true;
+        System.out.println(isAutumn);
+
+        boolean isOctober = false;
+        System.out.println(isOctober);
+
+        boolean isMay = false;
+        System.out.println(isMay);
+
+        boolean isHalloween = false;
+        System.out.println(isHalloween);
+
+        boolean isNovember = true;
+        System.out.println(isNovember);
+
+        boolean isMorning = false;
+        System.out.println(isMorning);
+
+    String task2 = "\nTask 2: Using all Java Operators create arithmetic operations for each data type\n";
+
+    System.out.println(task2);
+        byte add1 = (byte) (price1 + price2);
+        System.out.println(add1);
+
+        byte sub1 = (byte) (price4 - price3);
+        System.out.println(sub1);
+
+        byte mult1 = (byte) (price5 * price1);
+        System.out.println(mult1);
+
+        byte div1 = (byte) (price5 / discount1);
+        System.out.println(div1);
+
+        int mult2 = salary1 * 12;
+        System.out.println(mult2);
+
+        int sub2 = (salary2 * 12) - rent2;
+        System.out.println(sub2);
+
+        int add2 = coronaCases + coronaRec;
+        System.out.println(add2);
+
+        int div2 = coronaCases / 365;
+        System.out.println(div2);
+
+        short sub3 = (short) (savings1 - rent1);
+        System.out.println(sub3);
+
+        short add4 = (short) (cardBalance1 + cardBalance2);
+        System.out.println(add4);
+
+        short div4 = (short) (savings2 / 12);
+        System.out.println(div4);
+
+        short mult4 = (short) (rent2 / 30);
+        System.out.println(mult4);
+
+        short rem4 = (short) (savings2 % rent1);
+        System.out.println(rem4);
+
+        float add5 = temp1 + temp2;
+        System.out.println(add5);
+
+        float sub5 = temp2 - temp3;
+        System.out.println(sub5);
+
+        float rem5 = tempAvrg3 % tempAvrg;
+        System.out.println(rem5);
+
+        float mult5 = tempAvrg2 * 12;
+        System.out.println(mult5);
+
+        float div5 = tempAvrg3 / 100;
+        System.out.println(div5);
+
+        long add6 = shares1 + shares2;
+        System.out.println(add6);
+
+        long sub6 = shares3 - shares4;
+        System.out.println(sub6);
+
+        long div6 = seconds / 60;
+        System.out.println(div6);
+
+        long mult6 = seconds * 365;
+        System.out.println(mult6);
+
+        long rem6 = distSun % 1800;
+        System.out.println(rem6);
+
+    }
+
+}
